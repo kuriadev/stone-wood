c878c18ec4e26888d405a8fd1a1cac5cf65ebf70
kuriadev <neextdev@gmail.com>
2026-05-01 14:15:53 +0800

    Removing the Event/Function

--- types/booking.ts
+++ types/booking.ts
@@ -1,5 +1,5 @@
 export type BookingStatus = "On Hold" | "Confirmed" | "Completed" | "Cancelled";
-export type BookingPackage = "Day Tour" | "Day Tour + Room" | "Event / Function" | "On-Site Reservation";
+export type BookingPackage = "Day Tour" | "Day Tour + Room" | "On-Site Reservation";
 
 export interface Booking {
   id: string;
